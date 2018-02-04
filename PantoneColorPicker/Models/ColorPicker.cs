96ee230a0afe5f3bf1f6ffb6829750a347d7abc4
Corentin Altepe <corentin.altepe@gmail.com>
2018-02-04 12:32:44 +0100

    Deleted Unit tests, as unrelevant any more

--- PantoneColorPicker/Models/ColorPicker.cs
+++ PantoneColorPicker/Models/ColorPicker.cs
@@ -1,9 +1,4 @@
 ﻿using PantoneColorPicker.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PantoneColorPicker.Models
 {
